3a9f432121d0992f8da9ba14a8334479e0df6923
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-09 15:10:51 +0200

    fine esercizio mail

---
+++ mail/js/script.js
@@ -0,0 +1,34 @@
+//lista di email nell'array
+
+const email = ["giovanni@gmail.com","barbara@gmail.com","luca@gmail.com","massimo@gmail.com"];
+// debug
+console.log(email);
+
+const myInput = document.querySelector("input");
+console.log(myInput);
+
+
+const myButton = document.querySelector("button");
+console.log(myButton);
+
+const myOutput = document.querySelector(".risultato");
+console.log(myOutput);
+
+
+//chiedi al utente la sua email
+myButton.addEventListener("click",
+    function(){
+        let emailUser = myInput.value;
+        for (let i = 0; i < email.length; i++) {
+            if (email[i] === emailUser) {
+                console.log("sei entrato")
+                myOutput.innerHTML = "benvenuto nel nostro sito"
+            }
+        }
+    }
+
+)
+
+
+
+
