007cc2323eb31cc1eb24191e2c2c472a17166070
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-10 10:28:32 +0200

    correzzione esercizio!!!

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,33 +3,44 @@
 const email = ["giovanni@gmail.com","barbara@gmail.com","luca@gmail.com","massimo@gmail.com"];
 // debug
 console.log(email);
-
+// chiedi al utente la sua email
 const myInput = document.querySelector("input");
 console.log(myInput);
 
-
+// l'utente manda l'input 
 const myButton = document.querySelector("button");
 console.log(myButton);
-
+// output
 const myOutput = document.querySelector(".risultato");
 console.log(myOutput);
 
-
-//chiedi al utente la sua email
+let mail = false
+//controlla se l'email e presente nella lista 
 myButton.addEventListener("click",
     function(){
         let emailUser = myInput.value;
         for (let i = 0; i < email.length; i++) {
-            if (email[i] === emailUser) {
+            if (email[i] == emailUser) {
+                mail = true
                 console.log("sei entrato");
-                myOutput.innerHTML = '<h2>benvenuto nel nostro sito</h2>'
+                document.getElementById("risultato").innerHTML = "benvenuto nel sito";
                
             } else {
-                
                
+               console.log("non sei entrato");
             }
+
+        // se e presente accedi o no 
+
+        if( mail == true){
+            document.getElementById("risultato").innerHTML = "benvenuto nel sito";
+        } else {
+            document.getElementById("risultato").innerHTML = "non sei entrato";
         }
+
+
     }
+ }
 
 )
 
